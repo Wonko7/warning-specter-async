71dedca486eaa06ba3c9351aebf72390f64870bf
William <william@undefined.re>
2019-09-03 13:57:52 +0200

    test in go block

--- src/naked/core.cljs
+++ src/naked/core.cljs
@@ -2,11 +2,12 @@
   (:require
     [com.rpl.specter :as sp :refer [setval select-one]]
     [utils.asyncawait :refer [await] :refer-macros [async]]
+    [cljs.core.async :refer [chan <! >! timeout] :refer-macros [go go-loop] :as a]
     ))
 
 
 (defn start []
-  (async
+  (go
     (let [user-info  {:hello {:thing "hihi"}}
           user-info2 (setval [:hello :thing] "haha" user-info)]
       (println user-info)
