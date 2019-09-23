ea11258097fd6f157be1505d8533342b870aecc5
William <william@undefined.re>
2019-09-23 12:18:18 +0200

    compile error using specter's FIRST

--- src/naked/core.cljs
+++ src/naked/core.cljs
@@ -1,13 +1,16 @@
 (ns naked.core
   (:require
-    [com.rpl.specter :as sp :refer [setval select-one]]
+    [cljs.core.async :refer [chan <! >! timeout] :refer-macros [go go-loop] :as a]
+    [com.rpl.specter :as sp :refer [setval select-one FIRST]]
     [utils.asyncawait :refer [await] :refer-macros [async]]
     ))
 
+(def get-thing #(select-one [:hello FIRST :thing] %))
 
 (defn start []
-  (async
-    (let [user-info  {:hello {:thing "hihi"}}
-          user-info2 (setval [:hello :thing] "haha" user-info)]
-      (println user-info)
-      (println user-info2))))
+  (async ;; 1/ changing this into a go block compiles and works (node prints out hi)
+    (let [user-info  {:hello [{:thing "hi"}]}
+          thing      (select-one [:hello FIRST :thing] user-info)
+          ;;thing      (get-thing user-info) ;; 2/ with async: commenting the previous line and using this instead works, select-one needs to be compiled outside the async block
+          ]
+      (println thing))))
