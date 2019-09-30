28e205edf83cc34056ebbd100eedd5c110277538
William <william@undefined.re>
2019-09-30 13:36:23 +0200

    use set-val in a function in an async block

--- src/naked/core.cljs
+++ src/naked/core.cljs
@@ -10,7 +10,6 @@
 (defn start []
   (async ;; 1/ changing this into a go block compiles and works (node prints out hi)
     (let [user-info  {:hello [{:thing "hi"}]}
-          thing      (select-one [:hello FIRST :thing] user-info)
-          ;;thing      (get-thing user-info) ;; 2/ with async: commenting the previous line and using this instead works, select-one needs to be compiled outside the async block
+          get-thing  #(select-one [:hello FIRST :thing] %)
           ]
-      (println thing))))
+      (println (get-thing user-info)))))
