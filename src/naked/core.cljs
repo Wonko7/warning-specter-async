ad9ea713136ca58fe2af8e45b35acd31a38e655f
William <william@undefined.re>
2019-10-16 14:59:07 +0200

    broken shadow release

--- src/naked/core.cljs
+++ src/naked/core.cljs
@@ -1,15 +1,12 @@
 (ns naked.core
   (:require
-    [cljs.core.async :refer [chan <! >! timeout] :refer-macros [go go-loop] :as a]
-    [com.rpl.specter :as sp :refer [setval select-one FIRST]]
-    [utils.asyncawait :refer [await] :refer-macros [async]]
+    [sqlingvo.core        :as sql]
+    [sqlingvo.node.driver :as driver]
+    [sqlingvo.node.sync   :as db]
     ))
 
-(def get-thing #(select-one [:hello FIRST :thing] %))
-
 (defn start []
-  (async ;; 1/ changing this into a go block compiles and works (node prints out hi)
-    (let [user-info  {:hello [{:thing "hi"}]}
-          get-thing  #(select-one [:hello FIRST :thing] %)
-          ]
-      (println (get-thing user-info)))))
+  (let [db (db/db "postgresql://xxx:xxx@localhost:6543/xxx")
+        res @(sql/select db ['(max :version)]
+               (sql/from :meta))]
+    (println res)))
